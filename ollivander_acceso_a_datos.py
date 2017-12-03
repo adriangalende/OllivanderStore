3fc0695c122405730f011648b719ec705b30b4db
Adri <adriangalende@gmail.com>
2017-12-03 17:31:37 +0100

    comentada la funcion accesoCasosTexttest

---
+++ ollivander_acceso_a_datos.py
@@ -0,0 +1,113 @@
+def accesoCasosTexttest(matrizCasosTest, rutaAccesoFichero):
+    '''
+        inputs:
+            matrizCasosTest list
+            rutaAccesoFichero String
+
+        Busca linea a linea en fichero de texto que se encuentra en el argumento rutaAccesoFichero
+        La estructura de este fichero es la siguiente:
+
+            ---- dia 0 ---
+            name,sellIn,quality
+            String: nombre del item, Integer: vender en, Integer: calidad
+            ---- dia 1 ---
+            name,sellIn,quality
+            String: nombre del item, Integer: vender en, Integer: calidad
+
+        Por lo tanto, utilizamos 3 filtros de busqueda por este orden:
+
+            encuentra la palabra "day":  #cabecera inicio de cada uno de los dias
+                casosTestDia = []
+            si no, si la linea es un salto de linea: # ya no encuentra mas items
+                matrizCasosTest = [casosTestDia[]]
+            si no, si encuentra la palabra "name": # hemos encontrado la cabecera items
+                numeroPropiedadesItem = len(linea.split(','))
+            si no: # la linea corresponde a un item
+                item = lista con los valores de la linea divididos por el caracter ","
+                casosTestDia = [item x]
+        outputs:
+            Devuelve una lista que contiene una lista de listas con los items de cada dia
+            matrizCasosTest =>
+                [
+                    Dia0[
+                            [name,sellIn,quality],
+                            [name,sellIn,quality],
+                            [name,sellIn,quality],
+                            [name,sellIn,quality],
+                            [name,sellIn,quality]
+                        ],
+                    Dia1[
+                            [name,sellIn,quality],
+                            [name,sellIn,quality],
+                            [name,sellIn,quality],
+                            [name,sellIn,quality],
+                            [name,sellIn,quality]
+                        ],
+                    Dia n...
+
+    '''
+    try:
+        if not isinstance(rutaAccesoFichero, str):
+            raise ValueError
+        fichero = open(rutaAccesoFichero, 'r')
+    except FileNotFoundError:
+        print("Fichero no encontrado")
+        return []
+    except ValueError:
+        print("El nombre del fichero ha de ser un string")
+        return []
+    else:
+        matrizCasosTest = []
+        numeroPropiedadesItem = 0
+        for linea in fichero:
+            if linea.find("day") != -1:
+                casosTestDia = []
+            elif linea == "\n":
+                matrizCasosTest.append(casosTestDia)
+            elif linea.find("name") != -1:
+                numeroPropiedadesItem = len(linea.split(','))
+            else:
+                item = linea.rstrip().rsplit(',', maxsplit=numeroPropiedadesItem - 1)
+                casosTestDia.append(item)
+        fichero.close()
+        return matrizCasosTest
+
+
+def crearFicheroCasosTest(ficheroVolcadoCasosTest, matrizCasosTest):
+
+    try:
+        if not isinstance(ficheroVolcadoCasosTest, str):
+            raise ValueError
+        stdout = open(ficheroVolcadoCasosTest, 'w')
+    except ValueError:
+            print("La ruta de acceso al fichero ha de ser un string")
+    else:
+        for (offset, casosTestDia) in enumerate(matrizCasosTest):
+            stdout.write('-' * 5 + " Dia %d: " % offset + '-' * 5 + '\n')
+            for item in casosTestDia:
+                stdout.write(','.join(item) + '\n')
+        stdout.close()
+
+
+def mostrarCasosTest(matrizCasosTest):
+
+    for (offset, casosTestDia) in enumerate(matrizCasosTest):
+        print('-' * 5 + " Dia %d: " % offset + '-' * 5)
+        for item in casosTestDia:
+            print(item)
+
+
+if __name__ == "__main__":
+
+    rutaAccesoFichero = "./casos_tets.txt"
+    # rutaAccesoFichero = "stdout_bug_conjured.gr"
+
+    matrizCasosTest = []
+
+    matrizCasosTest = accesoCasosTexttest(matrizCasosTest, rutaAccesoFichero)
+
+    mostrarCasosTest(matrizCasosTest)
+
+    ficheroVolcadoCasosTest = "./stdout.txt"
+
+    crearFicheroCasosTest(ficheroVolcadoCasosTest, matrizCasosTest)
