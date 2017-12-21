8e791e1c6a5566bc7268733c591f32fab19bbc0c
Adri <adriangalende@gmail.com>
2017-12-21 13:04:49 +0100

    creada Interfaz, clase NormalItem y clase AgedBrie con requisitos de Kata

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,4 +1,4 @@
-class GildedRose(object):
+"""class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
@@ -33,7 +33,7 @@ class GildedRose(object):
                     if item.quality < 50:
                         item.quality = item.quality + 1
 
-
+"""
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -44,5 +44,44 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
+class Interfaz:
+    def updateQuality(self):
+        pass
 
-if __name__ == '__main__':
+
+class NormalItem(Item, Interfaz):
+    def setSellIn(self):
+        self.sell_in -= 1
+
+    def updateQuality(self):
+        if self.sell_in > 0:
+            self.setQuality(-1)
+        else:
+            # Once the sell by date has passed, Quality degrades twice as fast
+            self.setQuality(-2)
+        self.setSellIn()
+
+
+class AgedBrie(NormalItem):
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def setQuality(self, valor):
+        # Aged Brie" actually increases in Quality the older it gets
+        # The Quality of an item is never more than 50
+        # The Quality of an item is never negative
+        if self.quality + valor > 50:
+            self.quality = 50
+        elif self.quality + valor >= 0:
+            self.quality += valor
+        else:
+            self.quality = 0
+
+    def updateQuality(self):
+        if self.sell_in > 0:
+            self.setQuality(1)
+        else:
+            self.setQuality(2)
+        self.setSellIn()
