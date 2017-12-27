9584325fe280da60d1f0339b566f4f28211eedcd
Adri <adriangalende@gmail.com>
2017-12-27 10:32:09 +0100

    Añadida clase Sulfuras con casos test pasados

---
+++ sulfuras.py
@@ -0,0 +1,31 @@
+
+from Items import NormalItem
+
+
+class Sulfuras(NormalItem):
+
+    def __init__(self, name, sell_in, quality):
+        NormalItem.__init__(self, name, sell_in, quality)
+
+    def setQuality(self, valor):
+        self.quality = 80
+
+    def setSellIn(self):
+        pass
+
+    def updateItem(self):
+        self.setSellIn()
+
+
+if __name__ == '__main__':
+    from ollivander_acceso_a_datos import accesoCasosTexttest
+    itemList = ""
+    rutaAccesoFichero = "./stdout.gr.txt"
+    itemList = accesoCasosTexttest(itemList, rutaAccesoFichero)
+    sulfuras = Sulfuras(itemList[0][4][0], itemList[0][4][1], itemList[0][4][2])
+    for items in itemList[1:]:
+        for item in items:
+            if item[0] == sulfuras.getName() and item[1] == sulfuras.getSellIn():
+                sulfuras.updateItem()
+                assert sulfuras.getSellIn() == item[1] and sulfuras.getQuality() == item[2], "Error en día %d" % itemList.index(items)
+    print("todos casos test pasados!")
